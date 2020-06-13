a1b33f4e8ffe16a1f9b974cc92afcd2a5784e8ae
adventeurs <adventeurs@gmail.com>
2020-06-12 22:45:06 -0500

    User feature module

--- firestarter/src/app/app.module.ts
+++ firestarter/src/app/app.module.ts
@@ -7,13 +7,24 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { SharedModule } from "./shared/shared.module";
 import { HomePageComponent } from './home-page/home-page.component';
 
+import { environment } from '../environments/environment'
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
 @NgModule({
   declarations: [AppComponent, HomePageComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    SharedModule
+    SharedModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
