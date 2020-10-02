6259f898ca100979972562975a41e555598af654
Justin Miller <adventeurs@gmail.com>
2020-10-02 16:20:20 -0500

    parse xml to json in goodreads service

---
+++ LearnByLit/src/app/models/goodreads.ts
@@ -0,0 +1,5 @@
+export interface Shelf {
+  book_count: string;
+  id: string;
+  name: string;
+}
--- LearnByLit/src/app/services/goodreads.service.ts
+++ LearnByLit/src/app/services/goodreads.service.ts
@@ -1,20 +1,53 @@
 import { HttpClient } from "@angular/common/http";
-import { HttpClientModule } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
+
+import xml2js from "xml2js";
 import { environment } from "../../../src/environments/environment";
+
+import { Shelf } from "../models/goodreads";
 
 @Injectable({
   providedIn: "root",
 })
 export class GoodreadsService {
-  key: string = environment.goodreads.key;
+  private key: string = environment.goodreads.key;
+  private parser = new xml2js.Parser({
+    trim: true,
+    explicitArray: true,
+  });
 
   constructor(private http: HttpClient) {}
 
-  getShelves(uid: string | number) {
+  //
+  // // Get User Shelves
+  //
+  getShelves(uid: string | number): Observable<Shelf[]> {
     const url = "/goodreads/shelf/list.xml";
-    const param = { key: this.key, user_id: String(uid), page: "1" };
+    const params = { key: this.key, user_id: String(uid), page: "1" };
 
-    return this.http.get(url, { params: param, responseType: "text" });
+    return this.http
+      .get(url, { params, responseType: "text" })
+      .pipe(map((res) => this.parse(res)));
+  }
+
+  //
+  // // Parse XML For User Shelves
+  //
+  private parse(res) {
+    let shelf: Shelf[] = [];
+    this.parser.parseString(res, (err, res) => {
+      const { GoodreadsResponse } = res;
+      for (let s of GoodreadsResponse.shelves[0].user_shelf) {
+        shelf.push({
+          book_count: s.book_count[0]._,
+          name: s.name[0],
+          id: s.id[0]._,
+        });
+      }
+    });
+    return shelf;
   }
 }
